7ece21d2e92c5a8fffef144f79417a8c1d75b1a1
Ethan Mai <57975262+iAmEthanMai@users.noreply.github.com>
2020-12-05 12:53:36 -0500

    changed data

--- scripts/data_processing.py
+++ scripts/data_processing.py
@@ -9,10 +9,16 @@ from os.path import join
 # convert vac annotation format to darknet format
 def xml_to_darknet(path):
     root = ElementTree().parse(path)
-    img_path = root.find('filename').text.replace('png','txt')
 
-    with open('../data/txt/' + img_path, 'w') as f:
+    img_path = root.find('filename').text.replace('png','txt')
+    img_size = root.find('size')
+    width = int(img_size.find('width').text)
+    height = int(img_size.find('height').text)
+    
+    with open('../data/labels/' + img_path, 'w') as f:
         lines = []
+
+        
         for node in root.findall('object'):
             object_ = dict(class_=None, x=None, y=None, width=None, height=None)
 
@@ -26,24 +32,34 @@ def xml_to_darknet(path):
             else:
                 object_['class_'] = '2'
 
-
+            
+            
             # bounding box
             bnd_box = node.find("bndbox")
-            x_min = int(bnd_box[0].text)
-            y_min = int(bnd_box[1].text)
-            x_max = int(bnd_box[2].text)
-            y_max = int(bnd_box[3].text)
+            x_min = float(bnd_box[0].text)
+            y_min = float(bnd_box[1].text)
+            x_max = float(bnd_box[2].text)
+            y_max = float(bnd_box[3].text)
 
-            width = x_max - x_min
-            height = y_max - y_min
 
-            x = int((x_min + x_max)/2)
-            y = int((y_min + y_max)/2)
+            dw = float(1/width)
+            dh = float(1/height)
+
+            w = float(x_max - x_min)
+            h = float(y_max - y_min)
+
+            x = float((x_min + x_max)/2 -1)
+            y = float((y_min + y_max)/2 -1)
+
+            w = float(w * dw)
+            h = float(h * dh)
+            x = float(x * dw)
+            y = float(y * dh)
             
             object_['x'] = str(x)
             object_['y'] = str(y)
-            object_['width'] = str(width)
-            object_['height'] = str(height)
+            object_['width'] = str(w)
+            object_['height'] = str(h)
 
             line = object_['class_'] + ' ' + object_['x'] + ' ' + object_['y'] + ' ' + object_['width'] + ' ' + object_['height']
             
@@ -66,18 +82,19 @@ def process_data():
         
     for dirname, _, filenames in os.walk('../data/images'):
         for filename in filenames:
-            # img_paths.append(os.path.join('../mask-detection-dataset/data/images', filename))
-            img_paths.append(os.path.join('/content/mask-detection-dataset/data/images', filename))
-
+            # img_paths.append(os.path.join('../mask-detection-dataset/data/images', filename)) # local machine
+            img_paths.append(os.path.join('/content/mask-detection-dataset/data/images', filename)) # google colab
+            # img_paths.append(os.path.join('./mask-detection-dataset/data/images', filename)) # kaggle
+            
     # shuffle data
     random.shuffle(img_paths)
 
     # split
     train, validate, test = np.split(img_paths, [int(len(img_paths)*0.8), int(len(img_paths)*0.9)])
     
-    # print(len(train)) # training images set = 80% of all images
-    # print(len(validate)) # validating images set = 10% of all images
-    # print(len(test)) # testing images set = 10% of all images
+    # training images set = 80% of all images
+    # validating images set = 10% of all images
+    # testing images set = 10% of all images
 
     # write train.txt
     with open('../data/train.txt', 'w') as f:
